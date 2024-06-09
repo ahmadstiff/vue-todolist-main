db03b2392cdaeb9c4be896c1d50fbfb246ebb4db
ahmadstiff <royanuahmad313@gmail.com>
2024-06-09 11:57:28 +0700

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="d-flex align-items-center">
+        <RouterLink to="/" class="me-3 d-flex align-items-center">
+            <p class="mb-0">All</p>
+            <span class="badge text-bg-primary rounded-circle ms-1">{{ totalTasks }}</span>
+        </RouterLink>
+
+        <RouterLink to="/finish" class="me-3 d-flex align-items-center">
+            <p class="mb-0">Finished</p>
+        </RouterLink>
+
+        <RouterLink to="/available" class="me-3 d-flex align-items-center">
+            <p class="mb-0">Available</p>
+        </RouterLink>
+    </div>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+    props: {
+        totalTasks: {
+            type: Number,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
